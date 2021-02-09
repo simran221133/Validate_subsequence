49e1fea9638a5bddabb5a2b3b6a17c853eb2aff4
simran221133 <simran221133@gmail.com>
2021-02-08 22:57:04 -0500

    Update validate_sequence.py

--- validate_sequence.py
+++ validate_sequence.py
@@ -1,17 +1,17 @@
 function isValidSubsequence(array, sequence) {
-//pointer to array  
-let arrid=0;
-//pointer to sequence
-let seqid=0;
-
-//pointer less then lenghth of array and sequence
-while (arrid < array.length && seqid < sequence.length){
-       if (array[arrid] === sequence[seqid])
-         arrid=arrid+1;
-    }
-    seqid=seqid+1
-return sequence === sequence.length
+ # pointer to array
+ let arrid=0;
+ # pointer to sequence
+ let seqid=0;
+ 
+ # pointer less then lenghth of array and sequence
+ while (arrid < array.length && seqid < sequence.length){
+				if (array[arrid] === sequence[seqid]) seqid++
+	        arrid=arrid+1;
+				}
+        seqid=sequid+1
+return seqid === sequence.length
 }
 
-// Do not edit the line below.
 exports.isValidSubsequence = isValidSubsequence;
+
